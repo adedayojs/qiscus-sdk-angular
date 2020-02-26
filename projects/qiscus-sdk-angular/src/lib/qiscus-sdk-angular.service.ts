9da6917e6b1522c8d6296e1e8ebfab52b8f48644
Adedayo Adedunye <aadedunye@gidimobile.com>
2020-02-26 17:44:13 +0100

    Feature: added initialization to sdk

--- projects/qiscus-sdk-angular/src/lib/qiscus-sdk-angular.service.ts
+++ projects/qiscus-sdk-angular/src/lib/qiscus-sdk-angular.service.ts
@@ -1,10 +1,29 @@
 import { Injectable } from '@angular/core';
+import Qiscus from 'qiscus-sdk-core';
 
 @Injectable({
     providedIn: 'root'
 })
 export class QiscusSdkAngularService {
-    constructor() {}
+    private qiscus;
+    constructor() {
+        console.log(this.qiscus);
+        this.qiscus = this.qiscus || new Qiscus(); // This is to make sure that no other instance is rendered
+        console.log('****** I am initializing');
+        console.log(this.qiscus);
+    }
+    initialize(appId: string, options?: { [key: string]: string }) {
+        if (options) {
+            this.qiscus.init({
+                AppId: appId,
+                options: { ...options }
+            });
+            return;
+        }
+        this.qiscus.init({
+            AppId: appId
+        });
+    }
     logger(): void {
         console.log('This is quscus service logger');
     }
